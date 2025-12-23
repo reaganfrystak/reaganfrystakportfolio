fa382cbe3aae0bc0c7f27415b808f50e7d04c568
Reagan Frystak <fryst002@umn.edu>
2025-12-22 20:16:09 -0600

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/reaganfrystakportfolio/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+export default defineConfig(({ command }) => ({
+  plugins: [vue()],
+  // Use base path only for production builds, not dev server
+  base: command === 'build' ? '/reaganfrystakportfolio/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+    }
+  }
+}))
